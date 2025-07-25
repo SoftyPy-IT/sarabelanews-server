482805545f4969c32d85757089c74adef0980e02
Ibrahi Sikder <ibrahimsikder5033@gmail.com>
2025-07-26 02:14:50 +0600

    merge

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
 import { catchAsync } from '../../utils/catchAsync';
 
+let dddd
 
 export const validateRequest = (schema: AnyZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
